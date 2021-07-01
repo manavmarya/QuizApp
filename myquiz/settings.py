e5624516fdd588bad200b55e51c3937a37d5671f
manum1 <manu.marya@gmail.com>
2021-07-01 23:10:04 +0530

    Update for all heroku apps

--- myquiz/settings.py
+++ myquiz/settings.py
@@ -28,7 +28,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quizapp20041995.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
